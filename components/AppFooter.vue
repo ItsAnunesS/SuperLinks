976bd6c9038a77046da92ed94704d4f653ae4290
André Nunes <hello@anuness.dev>
2023-09-17 21:41:24 +0100

    Improve footer padding

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -4,7 +4,7 @@ const year = computed(() => new Date().getFullYear());
 </script>
 
 <template>
-  <footer class="daisyui-footer items-center justify-center daisyui-footer-center p-4 text-base-content">
+  <footer class="daisyui-footer items-center justify-center daisyui-footer-center pb-2 mt-8 p-4 text-base-content">
     <aside>
       <p>{{ t('footer.copyright', { name: t('app.name'), year }) }}</p>
     </aside>
