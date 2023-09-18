3e2a7aa5a9de6c8931897aabb47d83716a691395
André Nunes <hello@anuness.dev>
2023-09-18 01:18:51 +0100

    Add new twitter logo and color

--- components/AppSocialMedias.vue
+++ components/AppSocialMedias.vue
@@ -11,7 +11,7 @@ const socialMedias: socialMediaInterface[] = [
   {
     id: 'twitter',
     href: 'https://twitter.com/ItsAnunesS',
-    class: 'bg-[#00acee]',
+    class: 'bg-[#000]',
     icon: '<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 fill-current" fill="currentColor" viewBox="0 0 16 16"><path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z"/></svg>'
   },
   {
