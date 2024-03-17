4b05680b0e4a77e61dd679246b4508f160539418
André Nunes <hello@anuness.dev>
2024-03-17 22:16:33 +0000

    feat: add modules and plugin analyitcs with vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/ui',
+    '@nuxtjs/tailwindcss',
+    '@formkit/auto-animate/nuxt',
+    '@vant/nuxt',
+    '@nuxtjs/google-fonts',
+    '@dargmuesli/nuxt-cookie-control',
+    '@nuxtjs/seo',
+  ],
+  vant: {
+  },
+  googleFonts: {
+  },
+  site: {
+    url: 'https://links.anuness.dev',
+  }
 })
