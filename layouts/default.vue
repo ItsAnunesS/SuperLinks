fcd769e8e54749a92d427f9005b179a585305c59
André Nunes <hello@anuness.dev>
2024-03-31 05:07:54 +0100

    feat: add favicon

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,60 @@
 <script setup lang="ts">
-
+useHeadSafe({
+  link: [
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-touch-icon.png?v=323223",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon-32x32.png?v=323223",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicon-16x16.png?v=323223",
+    },
+    {
+      rel: "manifest",
+      href: "/manifest.webmanifest?v=323223",
+    },
+    {
+      rel: "mask-icon",
+      href: "/safari-pinned-tab.svg?v=323223",
+      color: "#e31c1c",
+    },
+    {
+      rel: "shortcut icon",
+      href: "/favicon.ico?v=323223",
+    },
+  ],
+  meta: [
+    {
+      name: "apple-mobile-web-app-title",
+      content: "André Nunes",
+    },
+    {
+      name: "application-name",
+      content: "André Nunes",
+    },
+    {
+      name: "msapplication-TileColor",
+      content: "#f41010"
+    },
+    {
+      name: "msapplication-TileImage",
+      content: "/mstile-144x144.png?v=323223",
+    },
+    {
+      name: "theme-color",
+      content: "#ffffff",
+    },
+  ]
+})
 </script>
 
 <template>
