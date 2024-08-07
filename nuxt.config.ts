4b2b871f2ca041998878e220c47e7ed4efcb0162
André Nunes <hello@anuness.dev>
2024-08-07 01:51:14 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  nitro: {
+    static: true
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@formkit/auto-animate/nuxt',
